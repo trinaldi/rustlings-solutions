3b4973e10923ebc0726e2ef7c5f183e835b4f74d
Tiago Rinaldi <trinaldi@gmail.com>
2020-08-30 00:24:45 -0300

    Finish result exercise

--- exercises/error_handling/result1.rs
+++ exercises/error_handling/result1.rs
@@ -1,8 +1,8 @@
 // result1.rs
 // Make this test pass! Execute `rustlings hint result1` for hints :)
 
-// I AM NOT DONE
-
+// THis semicolon rules don't make sense. Really.
+// WTF is this `if`, BTW? ffs
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
 
@@ -14,7 +14,13 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        Ok(PositiveNonzeroInteger(value as u64))
+        if value > 0 {
+            Ok(PositiveNonzeroInteger(value as u64))
+        } else if value < 0 {
+            Err(CreationError::Negative)
+        } else {
+            Err(CreationError::Zero)
+        }
     }
 }
 
